8ee1dfd338cd14a931d41b3d78ad5acac0320aad
James Cicenia <noreply>
2016-05-25 23:15:31 +0000

    Update files

---
+++ ui/category-list.reel/category-list.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/category-list.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class CategoryList
+ * @extends Component
+ */
+exports.CategoryList = Component.specialize(/** @lends CategoryList# */ {
+    constructor: {
+        value: function CategoryList() {
+            this.super();
+        }
+    }
+});
